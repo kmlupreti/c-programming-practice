1fbc121886508077bb6eff46c1fe9eb70e77ff53
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2024-10-27 15:16:28 +0545

    Refactor variable naming

--- ch-1/longest-line.c
+++ ch-1/longest-line.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-// maximum limit of input line
-#define MAX_LINE_LIMIT 100
+// maximum length of input line
+#define MAX_LINE_LENGTH 100
 // reads character and puts in line[] and returns its length
 int get_line(char line[]);
 // returns length of the line
@@ -9,22 +9,22 @@ int len(char line[]);
 void copy(char from[], char to[]);
 
 int main() {
-  char current_line[MAX_LINE_LIMIT];
-  char longest_line_found[MAX_LINE_LIMIT];
-  int max_length_found = 0;
+  char current_line[MAX_LINE_LENGTH];
+  char longest_line[MAX_LINE_LENGTH];
+  int longest_length = 0;
   int current_line_length;
 
   while ((current_line_length = get_line(current_line)) > 0) {
-    if (current_line_length > max_length_found) {
-      max_length_found = current_line_length;
-      copy(current_line, longest_line_found);
+    if (current_line_length > longest_length) {
+      longest_length = current_line_length;
+      copy(current_line, longest_line);
     }
   }
 
   // print if any valid longest line is found
-  if (max_length_found > 0) {
+  if (longest_length > 0) {
     printf("longest line is given below:\n");
-    printf("%s", longest_line_found);
+    printf("%s", longest_line);
   } else
     printf("no longest line found");
   return 0;
@@ -33,7 +33,7 @@ int main() {
 int get_line(char line[]) {
   int c, i;
   // add character to line string if its valid
-  for (i = 0; i < MAX_LINE_LIMIT - 1 && (c = getchar()) != '\n' && c != EOF;
+  for (i = 0; i < MAX_LINE_LENGTH - 1 && (c = getchar()) != '\n' && c != EOF;
        i++)
     line[i] = c;
   // add new line encountered at the end of the line
