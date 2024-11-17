8ba98a040c04768f4745a40d0e2db6eb48410de2
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2024-11-17 12:39:14 +0545

    Program to show bytes of data

--- show_bytes.c
+++ show_bytes.c
@@ -2,6 +2,11 @@
 typedef char *byte_ptr;
 void show_bytes(byte_ptr b, size_t len) {
   for (int i = 0; i < len; i++)
-    printf("%x", b[i]);
+    printf("%x\t", b[i]);
 }
-int main() { return 0; }
+int main() {
+  int x = 0x12345678;
+  show_bytes((byte_ptr)&x, sizeof(int));
+  printf("\n");
+  return 0;
+}
