ec7abff781f2bd0a0ab4c138235110a2b069c17e
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2024-12-05 18:53:40 +0545

    Use pointer based string copy

--- longest-line.c
+++ longest-line.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 // maximum length of input line
 #define MAX_LINE_LENGTH 100
-// reads character and puts in line[] and returns its length
-int get_line(char line[]);
-// returns length of the line
-int len(char line[]);
-// copy string (or array) of characters
-void copy(char from[], char to[]);
 
+// reads character and puts in line[] and returns its length
+int get_line(char line[]) {
+  int c, i;
+  // add character to line string if its valid
+  for (i = 0; i < MAX_LINE_LENGTH - 1 && (c = getchar()) != '\n' && c != EOF;
+       i++)
+    line[i] = c;
+  // add new line encountered at the end of the line
+  if (c == '\n') {
+    line[i] = c;
+    i++;
+  }
+  // put null character at the end of line to make it valid string
+  line[i] = '\0';
+
+  return i;
+}
+
+// copy string (or array) of characters
+void copy(char *from, char *to) {
+  while ((*to++ = *from++) != '\0')
+    ;
+}
 int main() {
   char current_line[MAX_LINE_LENGTH];
   char longest_line[MAX_LINE_LENGTH];
@@ -28,27 +45,4 @@ int main() {
   } else
     printf("no longest line found");
   return 0;
-}
-
-int get_line(char line[]) {
-  int c, i;
-  // add character to line string if its valid
-  for (i = 0; i < MAX_LINE_LENGTH - 1 && (c = getchar()) != '\n' && c != EOF;
-       i++)
-    line[i] = c;
-  // add new line encountered at the end of the line
-  if (c == '\n') {
-    line[i] = c;
-    i++;
-  }
-  // put null character at the string to make it valid string
-  line[i] = '\0';
-
-  return i;
-}
-
-void copy(char from[], char to[]) {
-  int i = 0;
-  while ((to[i] = from[i]) != '\0')
-    i++;
 }
