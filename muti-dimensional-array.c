0f494e854c686e806e7900fd12b4492a555de1ea
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2025-01-06 13:34:52 +0545

    Change argument from array to pointer to array

--- muti-dimensional-array.c
+++ muti-dimensional-array.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-// in argument a[n][m], n can be omitted as passed argument is pointer to
-// first row. But we need no. of ints in each row
-void print_array(int a[][3]) {
+// Here the passed argument is pointer to first row which is an array of
+// length 3. *a is wrapped around () as it has lower precedence than []
+void print_array(int (*a)[3]) {
   for (int i = 0; i < 4; i++)
     for (int j = 0; j < 3; j++)
       printf("position: (%d,%d) value: %d\n", i, j, a[i][j]);
