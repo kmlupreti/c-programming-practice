8da4e17979d672a8f578ef30df90a90a197b31d8
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2025-01-10 12:34:19 +0545

    Add program showing complicated declarations

---
+++ complicated-declarations.c
@@ -0,0 +1,23 @@
+// pointer to pointer to int
+int **p;
+
+// pointer to array of ints
+int (*q)[10];
+
+// array of pointers to int
+int *n[10];
+
+// function returning pointer to an integer
+int *f();
+
+// pointer to function returning an integer
+int (*g)();
+
+// pointer to function returning pointer to an integer
+int *(*h)();
+
+// function returning pointer to an array of integers
+int (*i[10])();
+
+// pointer to function returning pointer to an array of integers
+int *(*j[10])()
