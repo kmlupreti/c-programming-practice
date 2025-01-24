9034c59c90c8c27ff6d3a829d01bfbe2733b27dc
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2025-01-24 11:07:56 +0545

    re-factor whole code and remove bugs

--- favourite-distros-counter.c
+++ favourite-distros-counter.c
@@ -18,20 +18,45 @@ struct distro distro_list[] = {
 };
 
 int get_distro_name(char *name) {
-  while ((*name = getchar()) != EOF)
-    ;
+  char c;
+  int len = 0;
+  printf("enter distro name to vote:\n");
+  while ((c = getchar()) != EOF && c != '\n')
+    name[len++] = c;
+  // add null terminator and increment value of len
+  // only if name was populated
+  if (len > 0)
+    name[len++] = '\0';
+  return len;
 }
+
+void display_distros() {
+  printf("\nDistro list to vote from:\n");
+  for (int i = 0; i <= 10; i++) {
+    printf("%s\n", distro_list[i].name);
+  }
+}
+
 int main() {
   char *distro_name;
-  int len;
-  while ((len = get_distro_name(distro_name)) >= 0) {
+  int matched_index = -1;
+  display_distros();
+  while (get_distro_name(distro_name) > 0) {
     for (int i = 0; i < TOTAL_DISTROS; i++) {
-      if (strcasecmp(distro_name, (distro_list[i]).name))
+      if (strcasecmp(distro_name, (distro_list[i]).name) == 0) {
+        matched_index = i;
         (distro_list[i]).count++;
+      }
     }
+    if (matched_index >= 0) {
+      printf("You voted for \"%s\" distro\n", distro_list[matched_index].name);
+      matched_index = -1; // reset index
+    } else
+      printf("Error: distro with that name not found\n");
   }
   for (int i = 0; i < TOTAL_DISTROS; i++) {
     printf("Distro\t\tTotal votes\n");
     printf("%s\t\t%d", (distro_list[i]).name, (distro_list[i]).count);
   }
+  return 0;
 }
