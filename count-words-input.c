2dabf7e2fa5b001f03d1ad482b1a1e9ebde9c43c
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2025-01-27 15:28:17 +0545

    Complete the program

--- count-words-input.c
+++ count-words-input.c
@@ -1,31 +1,33 @@
-// This program counts the number of occurences of the words given from  the
-// input its uses binary search instead of linear search as it grows
-// quadratically with input i.e its time complexity is O(n^2)
-// For each word in input, the tree is traversed and compared with root node
-// If the input word is same as word of root node its count is incremented
-// If the word of root node is smaller then we go to left node
-// otherwise we go to right node and countinue this process.
+// This program counts the number of occurences of the words given from the
+// input.It uses binary tree data structure to store word and its count. It also
+// uses binary search instead of linear search as latter one grows quadratically
+// with input i.e its time complexity is O(n^2) For each word in input, the tree
+// is traversed and compared with root node If the input word is same as word of
+// root node its count is incremented If the word of root node is smaller then
+// we go to left node otherwise we go to right node and countinue this process.
 // We need to add new node if no node has same word as in input.
 // This process helps to keep track of input word count as well in building the
-// tree itself. The left node is always lexicographically smaller and right node
-// is greater than root node
+// tree itself. The word on left node is always lexicographically smaller than
+// word on root node while word on right node is greater
 
 #include <string.h>
 #define MAX_WORD 100
 #include <stdio.h>
+#include <stdlib.h>
 
 // node of a binary tree data structure
 // it holds word and its frequency count
 // it also points to the nodes on the either side
 struct Node {
-  char word[MAX_WORD];
+  char *word;
   int count;
   struct Node *left;
-  struct Node *Right;
+  struct Node *right;
 };
 
+// gets a new word from the user
 int get_word(char *word, int max_len) {
-  // handle invalid word and max_len arguments
+  // handle invalid arguments
   if (word == NULL || max_len <= 0)
     return 0;
   char c;
@@ -34,17 +36,61 @@ int get_word(char *word, int max_len) {
   while (len < max_len - 1 && (c = getchar()) != EOF && c != '\n')
     word[len++] = c;
   // add null terminator and increment value of len
-  // only if word was provided
+  // only if a valid word was provided
   if (len > 0)
     word[len] = '\0';
   return len;
 }
 
+// allocates memory for the new node
+struct Node *alloc_node() { return (struct Node *)malloc(sizeof(struct Node)); }
+
+// creates a copy of given word pointer and returns its pointer
+char *copy_word(char *word);
+
+// traverses through binary tree and does one of the following action for each
+// node: a) Initialize a new node with current word if there is no node b)
+// traverse on left side of the node if the current word is smaller than word on
+// the current node c) otherwise, traverse on right side of the node
+// It returns the pointer to the node which contains the given word
+struct Node *traverse_tree(struct Node *nodeptr, char *word) {
+  int cond;
+  if (nodeptr == NULL) {
+    // create a new node if there is no node pointed by nodeptr
+    nodeptr = alloc_node();
+    nodeptr->word = copy_word(word);
+    nodeptr->count = 1;
+    nodeptr->left = NULL;
+    nodeptr->right = NULL;
+  } else if ((cond = strcmp(word, nodeptr->word)) == 0) {
+    // increment word counter if node with same word is found
+    nodeptr->count++;
+  } else if (cond < 0) {
+    // traverse left node given word is smaller than word on the current node
+    traverse_tree(nodeptr->left, word);
+  } else if (cond > 0) {
+    // traverse right node given word is smaller than word on the current node
+    traverse_tree(nodeptr->right, word);
+  } else {
+    printf("error: unable to proceed!");
+  }
+  return nodeptr;
+}
+
+// display the given node and other nodes it contain
+// it recursively shows node on left and right side of the current node
+void show_node_tree(struct Node *node) {
+  if (node == NULL)
+    return;
+  show_node_tree(node->left);
+  printf("word: %s count:%d\n", node->word, node->count);
+  show_node_tree(node->right);
+}
+
 int main() {
-  struct Node *nodeptr;
+  struct Node *current_node;
   char word[MAX_WORD];
   while (get_word(word, MAX_WORD) > 0) {
-    // TODO: complete logic to traverse the tree and sorting it while adding new
-    // node whenever necessary
+    traverse_tree(current_node, word);
   }
 }
