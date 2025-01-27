166df6652712015c536fdaafaca47e1e04505e61
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2025-01-27 16:36:47 +0545

    Fix logic to get correct word count

--- get-word.c
+++ get-word.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 
 // gets a new word from the user
-int get_word(char *word, int max_len) {
-  // handle invalid arguments
-  if (word == NULL || max_len <= 0)
-    return 0;
+int get_word(char word[], int max_len) {
   char c;
   int len = 0;
   printf("enter a word:\n");
-  while (len < max_len - 1 && (c = getchar()) != EOF && c != '\n')
-    *word++ = c;
-  // add null terminator and increment value of len
-  // only if a valid word was provided
-  if (len > 0)
-    word[len] = '\0';
+  while (len < max_len - 1 && (word[len] = c = getchar()) != EOF && c != '\n')
+    len++;
+  // add null terminator at the end of word to make it a valid string
+  word[len] = '\0';
   return len;
 }
 int main() {
   char word[100];
   int len = get_word(word, 100);
-  printf("word: %s and length: %d\n", word, len);
+  printf("word: %s length: %d\n", word, len);
 }
