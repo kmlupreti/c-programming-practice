a80a3f63edf036e8b8cf8d944117a849a215ccdd
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2025-03-22 07:52:25 +0545

    Add concatenation program

---
+++ cat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+  char c;
+  if (argc == 1) {
+    while ((c = getchar()) != EOF)
+      putchar(c);
+  } else {
+    FILE *fp;
+    for (int i = 1; i < argc; i++) {
+      fp = fopen(argv[i], "r");
+      while ((c = getc(fp)) != EOF)
+        putchar(c);
+    }
+  }
+  return 0;
+}
